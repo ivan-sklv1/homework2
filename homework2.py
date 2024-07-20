46a4470f615616fda3abdc146277c5b0fcebbf87
Иван <132178381+ivan-sklv1@users.noreply.github.com>
2024-07-20 22:55:38 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework_complete = 12
+hours_spent = 1.5
+name = 'Python'
+time_for_one_task = hours_spent / homework_complete
+
+print(f'Курс: {name}, всего задач: {homework_complete},'
+      f'затрачено часов: {hours_spent}, среднее время выполнения: {time_for_one_task}')
